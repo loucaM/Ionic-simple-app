7f40a4eeb244b3880d8f4521c5332c4fa873b190
louca75 <louca.mendy@gmail.com>
2018-01-16 13:17:42 +0100

    alerte + favoris

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,21 +1,47 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the QuotesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Quote } from '../data/quote.interface';
+import { OnInit } from '@angular/core';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
 @IonicPage()
 @Component({
   selector: 'page-quotes',
   templateUrl: 'quotes.html',
 })
-export class QuotesPage {
+export class QuotesPage implements OnInit {
+  ngOnInit() {
+    this.quoteGroup = this.navParams.data;
+  }
+  quoteGroup: {category: string, quotes: Quote[], icon: string} ;
+  
+  constructor(public navCtrl: NavController, private navParams: NavParams, private alertCtrl: AlertController) {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    //data = les données qu'on a passées sur la page précédente
+    
+  }
+  ajouterAuxFavoris(){
+   const alerte =  this.alertCtrl.create({
+     title: 'Ajout aux favoris',
+     subTitle: 'êtes vous sur ?',
+     message: 'êtes vous sur de vouloir ajouter cette citation aux favoris ?',
+     buttons: [
+       {
+         text: 'Oui, allons-y !',
+         handler: () => {
+           console.log('Ok');
+         }
+       },
+       {
+        text: 'Non, j\'ai changé d\'avis' ,
+        handler: () => {
+          console.log('Annulé');
+        }
+       }
+     ]
+   });
+   alerte.present();
+
   }
 
   ionViewDidLoad() {
